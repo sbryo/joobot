88b7a4df9fd315dafe529a4850d10baa57d13433
shaked.braimok <shaked.braimok@startapp.com>
2016-04-15 20:02:23 +0300

    links fixed

--- index.py
+++ index.py
@@ -180,5 +180,5 @@ def public_append():
 
 if __name__ == "__main__":
     app.secret_key = "abcdefghijklmnoppqrstuvwxyz"
-    app.run(port=8080, host="0.0.0.0", debug=True)
+    app.run(port=8090, host="0.0.0.0", debug=True)
 
