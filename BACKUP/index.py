550e191aa018cb28552f5497818ba6607abd31d2
bshaked <shaked1817@gmail.com>
2016-05-03 14:57:04 +0300

    test

--- BACKUP/index.py
+++ BACKUP/index.py
@@ -1,116 +1,94 @@
 
 ### This Web-App Created BY SHAKED BRAIMOK ###
-####### APP NAME: My-News #######
+####### APP NAME: Dinero #######
 
-import functools
-
+import os
 import flask
+import subprocess
+import datetime
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+import json
+
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def start():
+	return flask.render_template('dinero-login.html')
+
+@app.route("/dinero")
+def dinero():
 	return flask.render_template('index.html')
 
-@app.route("/login")
-def main():
-    if "username" in flask.session:
-        return flask.redirect("/my_news")
-    return flask.render_template('login-index.html')
 
-@app.route("/loading", methods=['get', 'post'])
-def login():
-    if "username" in flask.request.form:
-        if "password" in flask.request.form and flask.request.form['username'] == "shaked" and flask.request.form['password'] == "1213123":
-            flask.session['username'] = "shaked"
+@app.route("/loading")
+def test():
+    #while results file is null
+	return flask.render_template('loading.html')
 
-            return flask.redirect("/")
-        else:
-            return "BAD USERNAME OR PASSWROD! PLEASE TRY AGAIN!"
-    else:
-        return flask.redirect("/login")
+#@app.route("/results")
+#@check_login
+#def results():
+ #   file = open("users-folders/shaked/Results.txt",'r')
+  #  lines = file.readlines()
+   # return flask.render_template('results.html',lines=lines)
 
+@app.route("/my-space")
+def my_space():
+	return flask.render_template('my-space.html')
 
-
-def check_login(func):
-    def wrapper(*args, **kwargs):
-        if "username" in flask.session:
-            return func(*args, **kwargs)
-        else:
-            return flask.redirect("/login")
-    return functools.update_wrapper(wrapper, func)
-
-
-@app.route("/my_dandash")
-@check_login
-def profile():
-	username = flask.session['username']
-	return flask.render_template('my-dandash.html')
-
-@app.route("/dandash_world")
-@check_login
-def world():
-	username = flask.session['username']
-	return flask.render_template('dandash-world.html')
-@app.route("/my_news")
-@check_login
-def my_news():
-    username = flask.session['username']
-   # os.mkdir("/app/users-folders/"+username)
-   # my_list = open("/app/users-folders/"+username+"/my_list.txt","a")
-   # my_list.close()
-   # my_history = open("/app/users-folders/"+username+"/my_history.txt","a")
-   # my_history.close()
-    return "Welcome %s!<br><a href=\"/logout\">Logout</a>" % flask.session['username']
-
-@app.route("/my_list_page")
-@check_login
-def my_list_page():
-    username = flask.session['username']
-    file = open("users-folders/"+username+"/my-list",'r')
-    lines = file.readlines()
-    return flask.render_template('my-list.html',lines=lines)
-
-@app.route("/my_list_page/appending",methods=['GET', 'POST'])
-@check_login
+@app.route("/search",methods=['GET', 'POST'])
 def append():
+	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+    	(out, err) = proc.communicate()
+	PATH=(out.split('\n'))[0]
 	if "add" in flask.request.form:
 		#data = str(flask.request.data)
 		text = flask.request.form['add']
     		processed_text = text.upper()
-		file = open("users-folders/shaked/my-list",'a')
-		file.write(processed_text+'\n')
+		file = open(PATH+"/users-folders/shaked/SearchFile.txt",'w')
+		file.write(processed_text)
 		file.close()
-		return flask.redirect("/my_list_page")
+
+	if "add2" in flask.request.form:
+		text = flask.request.form['add2']
+                processed_text = text.upper()
+                file = open(PATH+"/users-folders/shaked/SearchFile.txt",'a')
+                file.write(";"+processed_text)
+                file.close()
+	#os.system("python "+PATH+"/Dinero-System-Scripts/ebay.py")
+	ebay()
+	return flask.redirect("/results")
 		
-@app.route("/my_list_page/remove/<LINE>",methods=['GET','POST'])
-@check_login
+@app.route("/history/remove/<LINE>",methods=['GET','POST'])
 def remove(LINE):
+	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        PATH=(out.split('\n'))[0]
 	#if "Remove" in flask.request.form:
 	#LINE = flask.request.form['name']
-        username = flask.session['username']
-        LIST_FILE = open("users-folders/"+username+"/my-list",'r')
-        lines = LIST_FILE.readlines()
-	LIST_FILE.close()
-	LIST_FILE = open("users-folders/"+username+"/my-list",'w')
+        H_FILE = open(PATH+"/users-folders/shaked/History.txt",'r')
+        lines = H_FILE.readlines()
+	H_FILE.close()
+	H_FILE = open(PATH+"/users-folders/shaked/History.txt",'w')
         for line in lines:
         	if LINE not in line:
-			LIST_FILE.write(line)
+			H_FILE.write(line)
 		else:
 			continue	
-	LIST_FILE.close()
-        return flask.redirect("/my_list_page")
+	H_FILE.close()
+        return flask.redirect("/history")
 	
 
-
-
-@app.route("/my_news_page")
-@check_login
+@app.route("/history")
 def my_history_page():
+    proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    PATH=(out.split('\n'))[0]
     list = []
-    username = flask.session['username']
-    file = open("users-folders/"+username+"/my-news",'r')
+    file = open(PATH+"/users-folders/shaked/History.txt",'r')
     lines = file.readlines()
     file.close()
     for line in lines:
@@ -121,63 +99,102 @@ def my_history_page():
 		#link = x[1]
 		#titles.append(title)
 		#links.append(link)
-    return flask.render_template('my-news.html',list=list)
+    return flask.render_template('my-history.html',list=list)
 
-@app.route("/my_news_page/archive/<LINE>",methods=['GET','POST'])
-@check_login
-def archive(LINE):
+@app.route("/results/add_to_favorites/<LINE>",methods=['GET','POST'])
+def addtofavorites(LINE):
+	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        PATH=(out.split('\n'))[0]
         #if "Remove" in flask.request.form:
         #LINE = flask.request.form['name']
-        username = flask.session['username']
-        NEWS_FILE = open("users-folders/"+username+"/my-news",'r')
+        R_FILE = open(PATH+"/users-folders/shaked/Results.txt",'r')
         lines = NEWS_FILE.readlines()
-        NEWS_FILE.close()
-        NEWS_FILE = open("users-folders/"+username+"/my-news",'w')
-	ARCHIVE_FILE = open("users-folders/"+username+"/my-archive",'a')
+        R_FILE.close()
+	F_FILE = open(PATH+"/users-folders/shaked/Favorites.txt",'a')
         for line in lines:
                 if LINE not in line:
-                        NEWS_FILE.write(line)
+                        R_FILE.write(line)
                 else:
-			ARCHIVE_FILE.write(line)
-        NEWS_FILE.close()
-	ARCHIVE_FILE.close()
-        return flask.redirect("/my_news_page")
+			F_FILE.write(line)
+        R_FILE.close()
+	F_FILE.close()
+        return flask.redirect("/results")
 
 
-@app.route("/my_archive_page")
-@check_login
+@app.route("/favorites")
 def my_archive_page():
+    proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    PATH=(out.split('\n'))[0]
     list = []
-    username = flask.session['username']
-    ARCHIVE_FILE = open("users-folders/"+username+"/my-archive",'r')
-    lines = ARCHIVE_FILE.readlines()	
-    ARCHIVE_FILE.close()
+    F_FILE = open(PATH+"/users-folders/shaked/Favorites.txt",'r')
+    lines = F_FILE.readlines()	
+    F_FILE.close()
     for line in lines:
         if '=' in line:
 		x = line.split("=")
                 list.append(x)
-    return flask.render_template('my-archive.html',list=list)
+    return flask.render_template('my-favorites.html',list=list)
 
-@app.route("/my_archive_page/delete/<LINE>",methods=['GET','POST'])
-@check_login
-def delete(LINE):
+
+@app.route("/results")
+def my_archive_page2():
+    proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    PATH=(out.split('\n'))[0]
+    list = []
+    F_FILE = open(PATH+"/users-folders/shaked/Results.txt",'r')
+    lines = F_FILE.readlines()
+    F_FILE.close()
+    for line in lines:
+        if '=' in line:
+                x = line.split("=")
+                list.append(x)
+    return flask.render_template('results.html',list=list)
+
+
+
+@app.route("/favorites/delete/<LINE>",methods=['GET','POST'])
+def favorite_delete(LINE):
+	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        PATH=(out.split('\n'))[0]
         #if "Remove" in flask.request.form:
         #LINE = flask.request.form['name']
-        username = flask.session['username']
-        ARCHIVE_FILE = open("users-folders/"+username+"/my-archive",'r')
-        lines = ARCHIVE_FILE.readlines()
-        ARCHIVE_FILE.close()
-        ARCHIVE_FILE = open("users-folders/"+username+"/my-archive",'w')
+        F_FILE = open(PATH+"/users-folders/shaked/Favorites.txt",'r')
+        lines = F_FILE.readlines()
+        F_FILE.close()
+        F_FILE = open(PATH+"/users-folders/shaked/Favorites.txt",'w')
         for line in lines:
                 if LINE not in line:
-                        ARCHIVE_FILE.write(line)
+                        F_FILE.write(line)
                 else:
 			continue
-        ARCHIVE_FILE.close()
-        return flask.redirect("/my_archive_page")
+        F_FILE.close()
+        return flask.redirect("/favorites")
+
+@app.route("/history/delete/<LINE>",methods=['GET','POST'])
+def history_delete(LINE):
+	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        PATH=(out.split('\n'))[0]
+        #if "Remove" in flask.request.form:
+        #LINE = flask.request.form['name']
+        H_FILE = open(PATH+"/users-folders/shaked/History.txt",'r')
+        lines = H_FILE.readlines()
+        H_FILE.close()
+        H_FILE = open(PATH+"/users-folders/shaked/History.txt",'w')
+        for line in lines:
+                if LINE not in line:
+                        H_FILE.write(line)
+                else:
+                        continue
+        H_FILE.close()
+        return flask.redirect("/history")
+
 
 @app.route("/public")
-@check_login
 def public():
 	file = open("PUBLIC/publish-file",'r')
 	lines = file.readlines()
@@ -185,7 +202,6 @@ def public():
 	return flask.render_template('public.html',lines=lines)
 
 @app.route("/public/appending")
-@check_login
 def public_append():
         if "add" in flask.request.form:
                 #data = str(flask.request.data)
@@ -196,18 +212,67 @@ def public_append():
                 file.close()
                 return flask.redirect("/public")
 
-@app.route("/contact")
-def my_contact():
-	return flask.render_template('contact.html')
+def ebay():
+    ### FILES
+    proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    PATH=(out.split('\n'))[0]
 
-@app.route("/logout")
-def logout():
-    if "username" in flask.session:
-        del flask.session["username"]
-    return flask.redirect("/")
+    SEARCH_FILE = open(PATH+'/users-folders/shaked/SearchFile.txt','r')
+    KEYWORDS = SEARCH_FILE.read()
+    SEARCH_FILE.close()
+    RESULTS_FILE = open(PATH+'/users-folders/shaked/Results.txt','w')
+    HISTORY_FILE = open(PATH+'/users-folders/shaked/History.txt','a')
 
+### EBAY API
+    try:
+        api = Connection(appid='Shaked-B-976d-45bc-a23a-71ab251884fb',config_file=None)
+#response details:
+        response = api.execute('findItemsAdvanced',{'keywords':KEYWORDS})
+
+        assert(response.reply.ack == 'Success')
+        assert(type(response.reply.timestamp) == datetime.datetime)
+        assert(type(response.reply.searchResult.item) == list)
+
+        item = response.reply.searchResult.item[0]
+        assert(type(item.listingInfo.endTime) == datetime.datetime)
+        assert(type(response.dict()) == dict)
+    #print (((str(item).split(','))[10]).split(':')).split(',')
+    #print (str(item).split(','))
+
+
+        for ITEM in response.reply.searchResult.item:
+            try:
+                LIST = str(ITEM).split("'value':")
+                SHIPPING_PRICE = (LIST[1].split("'"))[1]
+                if SHIPPING_PRICE == '0.0':
+                    SHIPPING_PRICE = 'Free'
+
+                LIST = str(ITEM).split("'title':")
+                TITLE = (LIST[1].split("'"))[1]
+
+                LIST = str(ITEM).split("'viewItemURL':")
+                URL = (LIST[1].split("'"))[1]
+
+                LIST = str(ITEM).split("'galleryURL':")
+                IMG = (LIST[1].split("'"))[1]
+                LIST = (str(ITEM).split("'currentPrice':"))[1].split("'value':")
+                PRICE = (LIST[1].split("'"))[1]
+
+                RESULTS_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
+                HISTORY_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
+
+            except:
+                continue
+
+    except ConnectionError as e:
+        print(e)
+        print(e.response.dict())
+
+    RESULTS_FILE.close()
+    HISTORY_FILE.close()
 
 if __name__ == "__main__":
     app.secret_key = "abcdefghijklmnoppqrstuvwxyz"
-    app.run(port=8080, host="0.0.0.0", debug=True)
+    app.run(port=8090, host="0.0.0.0", debug=True)
 
