9b766a5548a887035c3277849353f044c56e1b58
bshaked <shaked1817@gmail.com>
2016-05-03 15:07:17 +0300

    test

--- index.py
+++ index.py
@@ -52,67 +52,16 @@ def append():
 		file.write(processed_text)
 		file.close()
 
-	    ### FILES
-    proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    PATH=(out.split('\n'))[0]
-
-    SEARCH_FILE = open(PATH+'/users-folders/shaked/SearchFile.txt','r')
-    KEYWORDS = SEARCH_FILE.read()
-    SEARCH_FILE.close()
-    RESULTS_FILE = open(PATH+'/users-folders/shaked/Results.txt','w')
-    HISTORY_FILE = open(PATH+'/users-folders/shaked/History.txt','a')
-
-### EBAY API
-    try:
-        api = Connection(appid='Shaked-B-976d-45bc-a23a-71ab251884fb',config_file=None)
-#response details:
-        response = api.execute('findItemsAdvanced',{'keywords':KEYWORDS})
-
-        assert(response.reply.ack == 'Success')
-        assert(type(response.reply.timestamp) == datetime.datetime)
-        assert(type(response.reply.searchResult.item) == list)
-
-        item = response.reply.searchResult.item[0]
-        assert(type(item.listingInfo.endTime) == datetime.datetime)
-        assert(type(response.dict()) == dict)
-    #print (((str(item).split(','))[10]).split(':')).split(',')
-    #print (str(item).split(','))
-
-
-        for ITEM in response.reply.searchResult.item:
-            try:
-                LIST = str(ITEM).split("'value':")
-                SHIPPING_PRICE = (LIST[1].split("'"))[1]
-                if SHIPPING_PRICE == '0.0':
-                    SHIPPING_PRICE = 'Free'
-
-                LIST = str(ITEM).split("'title':")
-                TITLE = (LIST[1].split("'"))[1]
-
-                LIST = str(ITEM).split("'viewItemURL':")
-                URL = (LIST[1].split("'"))[1]
-
-                LIST = str(ITEM).split("'galleryURL':")
-                IMG = (LIST[1].split("'"))[1]
-                LIST = (str(ITEM).split("'currentPrice':"))[1].split("'value':")
-                PRICE = (LIST[1].split("'"))[1]
-
-                RESULTS_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
-                HISTORY_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
-
-            except:
-                continue
-
-    except ConnectionError as e:
-        print(e)
-        print(e.response.dict())
-
-    RESULTS_FILE.close()
-    HISTORY_FILE.close()
-
+	if "add2" in flask.request.form:
+		text = flask.request.form['add2']
+                processed_text = text.upper()
+                file = open(PATH+"/users-folders/shaked/SearchFile.txt",'a')
+                file.write(";"+processed_text)
+                file.close()
+	#os.system("python "+PATH+"/Dinero-System-Scripts/ebay.py")
+	ebay()
 	return flask.redirect("/results")
-		
+
 @app.route("/history/remove/<LINE>",methods=['GET','POST'])
 def remove(LINE):
 	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
@@ -128,10 +77,10 @@ def remove(LINE):
         	if LINE not in line:
 			H_FILE.write(line)
 		else:
-			continue	
+			continue
 	H_FILE.close()
         return flask.redirect("/history")
-	
+
 
 @app.route("/history")
 def my_history_page():
@@ -180,7 +129,7 @@ def my_archive_page():
     PATH=(out.split('\n'))[0]
     list = []
     F_FILE = open(PATH+"/users-folders/shaked/Favorites.txt",'r')
-    lines = F_FILE.readlines()	
+    lines = F_FILE.readlines()
     F_FILE.close()
     for line in lines:
         if '=' in line:
