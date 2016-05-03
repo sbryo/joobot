a4742343d1264684a35508455a30c0bfb3a45637
bshaked <shaked1817@gmail.com>
2016-05-03 10:14:14 +0300

    test

--- Dinero-System-Scripts/ebay.py
+++ Dinero-System-Scripts/ebay.py
@@ -2,11 +2,25 @@ import datetime
 from ebaysdk.exception import ConnectionError
 from ebaysdk.finding import Connection
 import json
+import subprocess
+import os
 
+### FILES
+proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
+PATH=(out.split('\n'))[0]
+
+SEARCH_FILE = open('../users-folders/shaked/SearchFile.txt','r')
+KEYWORDS = SEARCH_FILE.read()
+SEARCH_FILE.close()
+RESULTS_FILE = open('../users-folders/shaked/Results.txt','w')
+HISTORY_FILE = open('../users-folders/shaked/History.txt','a')
+
+### EBAY API
 try:
     api = Connection(appid='Shaked-B-976d-45bc-a23a-71ab251884fb',config_file=None)
 #response details:
-    response = api.execute('findItemsAdvanced',{'keywords':'Armani Watch'})
+    response = api.execute('findItemsAdvanced',{'keywords':KEYWORDS})
 
     assert(response.reply.ack == 'Success')
     assert(type(response.reply.timestamp) == datetime.datetime)
@@ -23,6 +37,8 @@ try:
         try:
             LIST = str(ITEM).split("'value':")
             SHIPPING_PRICE = (LIST[1].split("'"))[1]
+            if SHIPPING_PRICE == '0.0':
+                SHIPPING_PRICE = 'Free'
 
             LIST = str(ITEM).split("'title':")
             TITLE = (LIST[1].split("'"))[1]
@@ -32,25 +48,20 @@ try:
 
             LIST = str(ITEM).split("'galleryURL':")
             IMG = (LIST[1].split("'"))[1]
-
             LIST = (str(ITEM).split("'currentPrice':"))[1].split("'value':")
             PRICE = (LIST[1].split("'"))[1]
 
+            #print "TITLE = "+TITLE
+            #print "PRICE = "+PRICE
+            #print "SHIPPING_PRICE = "+SHIPPING_PRICE
+            #print "URL = "+URL
+            #print "IMG = "+IMG
 
-            print "TITLE = "+TITLE
-            print "PRICE = "+PRICE
-            print "SHIPPING_PRICE = "+SHIPPING_PRICE
-            print "URL = "+URL
-            print "IMG = "+IMG
-            #FIELDS = str(ITEM).split(',')
-           # TITLE = ((FIELDS[4].split(':'))[1])[2:-1]
-           # SHIPPING = ((FIELDS[10].split(':'))[1])[2:-1]
-           # URL = ((FIELDS[10].split(':'))[1])[2:-1]
-            #print TITLE+" = "+SHIPPING
+            RESULTS_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
+            HISTORY_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
+
         except:
             continue
-
-
 
 
 
