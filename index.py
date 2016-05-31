6cbfcd8d2535cd05cddce37feeb2143ed71b24e1
bshaked <shaked1817@gmail.com>
2016-05-31 10:46:32 +0300

    test

--- index.py
+++ index.py
@@ -154,7 +154,7 @@ def my_archive_page():
 @app.route("/results")
 def get_results():
         try:
-                #subprocess.call("Dinero-System-Scripts/ebaydropbox.py")
+                subprocess.call("Dinero-System-Scripts/ebaydropbox.py")
                 proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
                 (out, err) = proc.communicate()
                 PATH=(out.split('\n'))[0]
