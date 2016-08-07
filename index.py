024005a3d4e46985074085fd3a65680743c0b024
sBryo <shaked1817@gmail.com>
2016-08-07 18:08:00 +0300

    Update index.py

--- index.py
+++ index.py
@@ -96,70 +96,65 @@ def remove(LINE):
 
 @app.route("/history")
 def my_history_page():
-    #proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
-    #(out, err) = proc.communicate()
-    #PATH=(out.split('\n'))[0]
-    list = []
-    app_key='4e3oofj6zqcx5dh'
-    app_secret='vaoz96wg81222c9'
-    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-    authorize_url = flow.start()
-    client = dropbox.client.DropboxClient('BH4cEdpiGmAAAAAAAAAAB5P3NEPXB2HO07UZJD56WRC5VfomuHI_Jz6Aa06YUUxl')
-    file, metadata = client.get_file_and_metadata('/Shaked/History.txt')
-    #file = open(PATH+"/users-folders/shaked/History.txt",'r')
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-	if '=' in line:
-		x = line.split(" = ")
-                list.append(x)
-		#title = x[0]
-		#link = x[1]
-		#titles.append(title)
-		#links.append(link)
-    return flask.render_template('my-history.html',list=list)
+    try:
+        list = []
+        client3 = MongoClient('ds019254.mlab.com',19254)
+        client3.search.authenticate('shakedinero','a57821688')
+        db_history = client3.history
+        cursor = db_history.history.shaked.find()
+    # Make list for html page
+        for document in cursor:
+            x = []
+            x.append(document['title'])
+            x.append(document['price'])
+            x.append(document['shipping'])
+            x.append(document['url'])
+            x.append(document['image'])
+            list.append(x)
+        return flask.render_template('my-history.html',list=list)
+    except:
+        return flask.render_template('404.html')
+
 
 @app.route("/results/add_to_favorites/<LINE>",methods=['GET','POST'])
 def addtofavorites(LINE):
-	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        PATH=(out.split('\n'))[0]
-        #if "Remove" in flask.request.form:
-        #LINE = flask.request.form['name']
-        R_FILE = open(PATH+"/users-folders/shaked/Results.txt",'r')
-        lines = NEWS_FILE.readlines()
-        R_FILE.close()
-	F_FILE = open(PATH+"/users-folders/shaked/Favorites.txt",'a')
-        for line in lines:
-                if LINE not in line:
-                        R_FILE.write(line)
-                else:
-			F_FILE.write(line)
-        R_FILE.close()
-	F_FILE.close()
-        return flask.redirect("/results")
+    client4 = MongoClient('ds019254.mlab.com',19254)
+    client4.search.authenticate('shakedinero','a57821688')
+    db_favorites = client4.favorites
 
-
+    client = MongoClient('ds019254.mlab.com',19254)
+    client.results.authenticate('shakedinero','a57821688')
+    db_results = client.results
+    cursor = db.results.shaked.find()
+    for doc in cursor:
+        if LINE in doc['title']:
+            db_favorites.favorites.shaked.insert(doc)
+        else:
+            continue
+    return flask.redirect("/results")
+            
+    
+    
 @app.route("/favorites")
 def my_archive_page():
-    proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    PATH=(out.split('\n'))[0]
-    list = []
-    #F_FILE = open(PATH+"/users-folders/shaked/Favorites.txt",'r')
-    app_key='4e3oofj6zqcx5dh'
-    app_secret='vaoz96wg81222c9'
-    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-    authorize_url = flow.start()
-    client = dropbox.client.DropboxClient('BH4cEdpiGmAAAAAAAAAAB5P3NEPXB2HO07UZJD56WRC5VfomuHI_Jz6Aa06YUUxl')
-    F_FILE, metadata = client.get_file_and_metadata('/Shaked/Favorites.txt')
-    lines = F_FILE.readlines()
-    F_FILE.close()
-    for line in lines:
-        if '=' in line:
-		x = line.split(" = ")
-                list.append(x)
-    return flask.render_template('my-favorites.html',list=list)
+    try:
+        client4 = MongoClient('ds019254.mlab.com',19254)
+        client4.search.authenticate('shakedinero','a57821688')
+        db_favorites = client4.favorites
+        cursor = db_favorites.favorites.shaked.find()
+        # Make list for html page
+        for document in cursor:
+            x = []
+            x.append(document['title'])
+            x.append(document['price'])
+            x.append(document['shipping'])
+            x.append(document['url'])
+            x.append(document['image'])
+            list.append(x)
+        return flask.render_template('my-favorites.html',list=list)
+    except:
+        return flask.render_template('404.html')
+    
 
 @app.route("/results")
 def get_results():
@@ -170,22 +165,14 @@ def get_results():
                 PATH=(out.split('\n'))[0]
                 list = []
                 os.system("python "+PATH+"/Dinero-System-Scripts/Dinero2Mongo.py")
-              #  app_key='4e3oofj6zqcx5dh'
-              #  app_secret='vaoz96wg81222c9'
-              #  flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-              #  authorize_url = flow.start()
-              #  client = dropbox.client.DropboxClient('BH4cEdpiGmAAAAAAAAAAB5P3NEPXB2HO07UZJD56WRC5VfomuHI_Jz6Aa06YUUxl')
 
-               # F_FILE, metadata = client.get_file_and_metadata('/Shaked/Results.txt')
-                #lines = F_FILE.readlines()
-                #F_FILE.close()
                 x = []  ### This is the list for html
                 client = MongoClient('ds019254.mlab.com',19254)
                 client.results.authenticate('shakedinero','a57821688')
                 db = client.results
                 cursor = db.results.shaked.find()
 
-################## Need to change the list of the HTML table ###############
+#Make list for html page
                 for document in cursor:
                     x = []
                     x.append(document['title'])
@@ -266,92 +253,6 @@ def public_append():
                 file.close()
                 return flask.redirect("/public")
 
-
-#@app.route("/parsing")
-#def pyscript()
-#    os.system("python Dinero-System-Scripts/ebay+dropbox.py")
-#    return flask.redirect("/results")
-
-
-#@app.route("/parsing")
-#def ebay():
-#Dropbox Connection
-#    app_key='4e3oofj6zqcx5dh'
-#    app_secret='vaoz96wg81222c9'
-#    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-#    authorize_url = flow.start()
-#    client = dropbox.client.DropboxClient('BH4cEdpiGmAAAAAAAAAAB5P3NEPXB2HO07UZJD56WRC5VfomuHI_Jz6Aa06YUUxl')#
-
-### FILES
-    #proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
-    #(out, err) = proc.communicate()
-    #PATH=(out.split('\n'))[0]
-
-    #SEARCH_FILE, metadata = client.get_file_and_metadata('/Shaked/SearchFile.txt')
-    #KEYWORDS = SEARCH_FILE.read()
-    #SEARCH_FILE.close()
-    #print KEYWORDS
-    #RESULTS_FILE = open(PATH+'users-folders/shaked/Results.txt','w')
-    #HISTORY_FILE = open(PATH+'users-folders/shaked/History.txt','a')
-
-### EBAY API
-    #try:
-    #    api = Connection(appid='Shaked-B-976d-45bc-a23a-71ab251884fb',config_file=None)
-#response details:
-     #   response = api.execute('findItemsAdvanced',{'keywords':KEYWORDS})
-
-      #  assert(response.reply.ack == 'Success')
-       # assert(type(response.reply.timestamp) == datetime.datetime)
-        #assert(type(response.reply.searchResult.item) == list)
-
-        #item = response.reply.searchResult.item[0]
-        #assert(type(item.listingInfo.endTime) == datetime.datetime)
-        #assert(type(response.dict()) == dict)
-        #print (((str(item).split(','))[10]).split(':')).split(',')
-        #print (str(item).split(','))
-
-
-        #for ITEM in response.reply.searchResult.item:
-        #    try:
-        #        LIST = str(ITEM).split("'value':")
-        #        SHIPPING_PRICE = (LIST[1].split("'"))[1]
-        #        if SHIPPING_PRICE == '0.0':
-        #            SHIPPING_PRICE = 'Free'
-
-        #        LIST = str(ITEM).split("'title':")
-        #        TITLE = (LIST[1].split("'"))[1]
-
-              #  LIST = str(ITEM).split("'viewItemURL':")
-              #  URL = (LIST[1].split("'"))[1]
-
-               # LIST = str(ITEM).split("'galleryURL':")
-               # IMG = (LIST[1].split("'"))[1]
-               # LIST = (str(ITEM).split("'currentPrice':"))[1].split("'value':")
-               # PRICE = (LIST[1].split("'"))[1]
-
-               # RESULTS_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
-               # HISTORY_FILE.write(TITLE+" = "+PRICE+" = "+SHIPPING_PRICE+" = "+URL+" = "+IMG+'\n')
-
-            #except:
-            #    continue
-
-        #RESULTS_FILE.close()
-        #HISTORY_FILE.close()
-
-        #r_file=open(PATH+"users-folders/shaked/Results.txt",'r')
-        #r = r_file.read()
-       # response = client.put_file('/shaked/Results.txt', r,overwrite=True)
-
-        #h_file=open(PATH+"users-folders/shaked/History.txt",'r')
-        #h=h_file.read()
-        #response = client.put_file('/shaked/History.txt', h,overwrite=True)
-
-
-#    except ConnectionError as e:
-#        print(e)
-#        print(e.response.dict())
-
- #   return flask.redirect("/results")
 
 @app.errorhandler(404)
 def page_not_found(e):
