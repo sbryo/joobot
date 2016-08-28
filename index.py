4032743b4cc19f597edd6a9f5aad2cfe9533128c
sbryo <shaked1817@gmail.com>
2016-08-28 08:58:48 +0300

    Revert "Update index.py"
    This reverts commit be84527764e8241f1c28a77d02be9e9e3d490185.

--- index.py
+++ index.py
@@ -66,18 +66,10 @@ def facebook_authorized(resp):
 
 def check_login(func):
 	def wrapper(*args, **kwargs):
-        	if ("username" in flask.session):
+        	if ("username" in flask.session) or (session['logged_in']==True):
             		return func(*args, **kwargs)
-                else:
-                        return flask.redirect("/")
-                try:
-                        if (session['logged_in']==True):
-                                return func(*args, **kwargs)
-                         
-        	        else:
-            		        return flask.redirect("/")
-                except:
-                        print "z"
+        	else:
+            		return flask.redirect("/")
 	return functools.update_wrapper(wrapper, func)
 
 @app.route("/")
