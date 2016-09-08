41f41bbc1353384e522f517ab63730d914f89065
sBryo <shaked1817@gmail.com>
2016-09-08 20:18:26 +0300

    Update index.py

--- index.py
+++ index.py
@@ -669,8 +669,9 @@ def logout():
     		return flask.redirect("/")
     	if (session['logged_in']==True):
     		pop_login_session()
-    		return redirect(url_for('joobot'))
-        	#return flask.redirect("/")
+    		session['logged_in']=False
+    		#return redirect(url_for('joobot'))
+        	return flask.redirect("/")
     	else:
     		return flask.redirect("/")
 
