ca6961023cef0ce8b20cdad734a9ca074f5132cd
sBryo <shaked1817@gmail.com>
2016-12-18 14:45:51 +0200

    Update index.py

--- index.py
+++ index.py
@@ -138,22 +138,22 @@ def signing():
         		cursor = db.users.find()
         		email = flask.request.form['email']
         		password = flask.request.form['password']
-        		for doc in cursor:
+			for doc in cursor:
         			### if user already exists
-            			if "password" in flask.request.form and str(email.lower()) == (str(doc['email']).lower()):
+				if "password" in flask.request.form and str(email.lower()) == (str(doc['email']).lower()):
 					x=x+1
             				return flask.redirect("/")
             			### Create user
-            		if x==0:
-                        	if (validate_email(str(email.lower())))==True:
-                            		ID=id_generator()
+			if x==0:
+				if (validate_email(str(email.lower())))==True:
+					ID=id_generator()
             			    	j=json.loads('{"email":"'+email.lower()+'","password":"'+password+'","ID":'+ID+'}')
             			    	db.users.insert(j)
                             		msg = Message("Accept Sign-Up to Shops !",sender="shops@app.com",recipients=[email])
                             		msg.body = "Dear "+str(email.split("@")[0])+", to accept the sign-up please click on the link: http://joobot-web.herokuapp.com/accept_signup/"+id+"   \n Enjoy !"
             			    #flask.session['username'] = email
                             		mail.send(msg)
-			 	return flask.render_template("after-signup.html")
+				return flask.render_template("after-signup.html")
             			#return flask.redirect("/joobot")
 
         	except:
