1a2eb6b4ee0bc0adaf265d9e5f247d5bfd682cdf
sBryo <shaked1817@gmail.com>
2016-12-19 08:00:03 +0200

    Update index.py

--- index.py
+++ index.py
@@ -164,10 +164,15 @@ def signing():
 				ID=id_generator()
 				j=json.loads('{"email":"'+email.lower()+'","password":"'+password+'","ID":"'+ID+'"}')
 				db.users.insert(j)
-				msg = Message("Accept Sign-Up to Shops !",sender='applicationshops@gmail.com',recipients=[email])
-				msg.body = "Dear "+str(email.split("@")[0])+", to accept the sign-up please click on the link: http://joobot-web.herokuapp.com/accept_signup/"+ID+"   \n Enjoy !"
+				s = smtplib.SMTP_SSL("smtp.gmail.com", 587)
+				s.starttls()
+				s.login('applicationshops@gmail.com', 'abc5678910')
+				s.sendmail('applicationshops@gmail.com', email, 'test')
+				s.close()
+				#msg = Message("Accept Sign-Up to Shops !",sender='applicationshops@gmail.com',recipients=[email])
+				#msg.body = "Dear "+str(email.split("@")[0])+", to accept the sign-up please click on the link: http://joobot-web.herokuapp.com/accept_signup/"+ID+"   \n Enjoy !"
 			    #flask.session['username'] = email
-				mail.send(msg)
+				#mail.send(msg)
 				return flask.render_template("after-signup.html")
 			else:
 				return flask.redirect("/signup")
